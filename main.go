b85f60475a6eca31c3f472f4e0e97bd1f86ee963
Khiem Le <theinnocentman1@gmail.com>
2022-12-10 11:15:09 +0700

    Add viper to load config from .env file

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/khiemledev/simple_bank_golang/api"
 	db "github.com/khiemledev/simple_bank_golang/db/sqlc"
+	"github.com/khiemledev/simple_bank_golang/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	serverAddress = "0.0.0.0:8080"
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", conn)
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server:", err)
 	}
