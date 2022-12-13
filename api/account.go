757976a4faeb1e475e91c87c203de693e8ff4fe1
Khiem Le <theinnocentman1@gmail.com>
2022-12-14 01:10:42 +0700

    Add unit test for user

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	db "github.com/khiemledev/simple_bank_golang/db/sqlc"
+	"github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -28,6 +29,16 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, args)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				{
+					ctx.JSON(http.StatusForbidden, errorResponse(err))
+					return
+				}
+			}
+		}
+
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
