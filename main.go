347270ae6fcf53e85a3d9a7bfda8ea0150f2c819
Khiem Le <theinnocentman1@gmail.com>
2022-12-24 20:48:19 +0700

    Add OpenAPI (aka Swagger)

--- main.go
+++ main.go
@@ -64,6 +64,9 @@ func startGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
+	fs := http.FileServer(http.Dir("./doc/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot create listener:", err)
