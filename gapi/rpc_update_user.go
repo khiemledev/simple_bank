507d08902ae543712b553f81bb0ec9a34528716f
Khiem Le <theinnocentman1@gmail.com>
2022-12-25 10:58:35 +0700

    Add route to update user rpc

---
+++ gapi/rpc_update_user.go
@@ -0,0 +1,84 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+
+	db "github.com/khiemledev/simple_bank_golang/db/sqlc"
+	"github.com/khiemledev/simple_bank_golang/pb"
+	"github.com/khiemledev/simple_bank_golang/util"
+	"github.com/khiemledev/simple_bank_golang/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	violations := validateUpdateUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
+	args := db.UpdateUserParams{
+		Username: req.GetUsername(),
+		FullName: sql.NullString{
+			String: req.GetFullName(),
+			Valid:  req.FullName != nil,
+		},
+		Email: sql.NullString{
+			String: req.GetEmail(),
+			Valid:  req.Email != nil,
+		},
+	}
+
+	if req.Password != nil {
+		hashedPassword, err := util.HashPassword(req.GetPassword())
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "hash password error")
+		}
+
+		args.HashedPassword = sql.NullString{
+			String: hashedPassword,
+			Valid:  true,
+		}
+	}
+
+	user, err := server.store.UpdateUser(ctx, args)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Error(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to update user")
+	}
+
+	rsp := &pb.UpdateUserResponse{
+		User: convertUser(user),
+	}
+	return rsp, nil
+}
+
+func validateUpdateUserRequest(req *pb.UpdateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if req.Email != nil {
+		if err := val.ValidateEmail(req.GetEmail()); err != nil {
+			violations = append(violations, fieldViolation("email", err))
+		}
+	}
+
+	if req.FullName != nil {
+		if err := val.ValidateFullName(req.GetFullName()); err != nil {
+			violations = append(violations, fieldViolation("full_name", err))
+		}
+	}
+
+	if req.Password != nil {
+		if err := val.ValidatePassword(req.GetPassword()); err != nil {
+			violations = append(violations, fieldViolation("password", err))
+		}
+	}
+
+	return violations
+}
--- val/validator.go
+++ val/validator.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	isValidUsername = regexp.MustCompile(`^[a-z0-9_]+$`).MatchString
-	isValidFullName = regexp.MustCompile(`^[a-zA-Z0-9\\s]+$`).MatchString
+	isValidFullName = regexp.MustCompile(`^[a-zA-Z0-9\s]+$`).MatchString
 )
 
 func ValidateString(value string, minLength, maxLength int) error {
